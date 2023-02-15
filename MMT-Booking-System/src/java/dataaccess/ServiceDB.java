b4732f341f3a5e809fd5c5ebb25b738abadb181f
j-albento <joannamarie.albento@gmail.com>
2023-02-15 14:59:20 -0700

    Added ServiceDB and MassageService

---
+++ MMT-Booking-System/src/java/dataaccess/ServiceDB.java
@@ -0,0 +1,80 @@
+package dataaccess;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import models.Service;
+
+/**
+ *
+ * @author Joanna
+ */
+public class ServiceDB {
+    
+    public List<Service> getAll() {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            return em.createNamedQuery("Service.findAll", Service.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Service getService(int serviceId) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            Service service = em.find(Service.class, serviceId);
+            return service;
+        } finally {
+            em.close();
+        }
+    }
+    public void insert(Service service) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        
+        getAll().add(service);
+        
+        try {
+            et.begin();
+            em.persist(service);
+            em.merge(service);
+            et.commit();
+        } catch (Exception e) {
+            et.rollback();
+        } finally {
+            em.close();
+        }
+    }
+    public void update(Service service) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        
+        try {
+            et.begin();
+            em.merge(service);
+            et.commit();
+        } catch (Exception e) {
+            et.rollback();
+        } finally {
+            em.close();
+        }
+    }
+    public void delete(Service service) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        
+        getAll().remove(service);
+        
+        try {
+            et.begin();
+            em.remove(em.merge(service));
+            et.commit();
+        } catch (Exception e) {
+            et.rollback();
+        } finally {
+            em.close();
+        }
+    }
+}
---
+++ MMT-Booking-System/src/java/services/MassageService.java
@@ -0,0 +1,45 @@
+package services;
+
+import dataaccess.ServiceDB;
+import java.util.List;
+import models.Service;
+
+/**
+ *
+ * @author Joanna
+ */
+public class MassageService {
+    
+    public List<Service> getAll() throws Exception {
+        ServiceDB servdb = new ServiceDB();
+        
+        return servdb.getAll();
+    }
+    public Service get(int serviceId) throws Exception {
+        ServiceDB servdb = new ServiceDB();
+        
+        return servdb.getService(serviceId);
+    }
+    public void insert(int serviceId, String type, String description, double cost) throws Exception {
+        ServiceDB servdb = new ServiceDB();
+        Service service = new Service(0, type, description, cost);
+        
+        servdb.insert(service);
+    }
+    public void update(int serviceId, String type, String description, double cost) throws Exception {
+        ServiceDB servdb = new ServiceDB();
+        Service service = servdb.getService(serviceId);
+        
+        service.setServiceType(type);
+        service.setServiceDesc(description);
+        service.setServiceCost(serviceId);
+        
+        servdb.update(service);
+    }
+    public void delete(int serviceId) throws Exception {
+        ServiceDB servdb = new ServiceDB();
+        Service service = servdb.getService(serviceId);
+        
+        servdb.delete(service);
+    }
+}
