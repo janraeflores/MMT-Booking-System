241fbbd6f18b33e029a36d888b1480ce9ba39038
j-albento <joannamarie.albento@gmail.com>
2023-02-16 00:01:37 -0700

    Partially working booking functionality

--- MMT-Booking-System/src/java/servlets/ReservationServlet.java
+++ MMT-Booking-System/src/java/servlets/ReservationServlet.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
 
+import dataaccess.AppointmentDB;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -15,9 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import models.Account;
+import models.Appointment;
 import models.Client;
+import models.Service;
 import services.AccountService;
+import services.AppointmentService;
 import services.ClientService;
+import services.MassageService;
+import services.Validate;
 
 /**
  *
@@ -34,23 +35,57 @@ public class ReservationServlet extends HttpServlet {
         
         AccountService as = new AccountService();
         ClientService cs = new ClientService();
+        MassageService ms = new MassageService();
         
         try {
             Account account = as.get(username);
             Client client = cs.get(username);
+            
+            request.setAttribute("service", ms.getAll());
             request.setAttribute("account", account);
             request.setAttribute("client", client);
+            
         } catch (Exception ex) {
             Logger.getLogger(AccountServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
         getServletContext().getRequestDispatcher("/WEB-INF/Reservation.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+            
+        AccountService as = new AccountService();
+        ClientService cs = new ClientService();
+        AppointmentService apptserv = new AppointmentService();
+        MassageService ms = new MassageService();
+       
+        try {
+            Client client = cs.get(username);
+           
+            int serviceType = Integer.parseInt(request.getParameter("s-type"));
+            String serviceDuration = request.getParameter("s-duration"); 
+            //String selectedDate = request.getParameter("");
+            //Date startTime = null;
+            //Date endTime = null;
+            String address = request.getParameter("u-address");
 
+            apptserv.insert(client, serviceType, address);
+
+            int length = apptserv.getAll(client.getClientId()).size();
+            Appointment appt = apptserv.getAll(client.getClientId()).get(length - 1);
+            
+            request.setAttribute("appointment", appt);
+            request.setAttribute("client", client);
+            request.setAttribute("duration", serviceDuration);
+            request.setAttribute("service", ms.getAll());
+        } catch (Exception ex) {
+            Logger.getLogger(ReservationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/Reservation.jsp").forward(request, response);
     }
 
 }
