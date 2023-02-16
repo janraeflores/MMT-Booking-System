90269cd0161a83e317c249ecacb78d317c868d29
j-albento <joannamarie.albento@gmail.com>
2023-02-16 15:13:20 -0700

    Updated

--- MMT-Booking-System/src/java/dataaccess/AppointmentDB.java
+++ MMT-Booking-System/src/java/dataaccess/AppointmentDB.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import models.Appointment;
@@ -7,6 +8,36 @@ import models.Client;
 
 public class AppointmentDB {
     
+    public List<Appointment> getAll() throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            return em.createNamedQuery("Appointment.findAll", Appointment.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+    public List<Appointment> getAll(int clientId) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            Client client = em.find(Client.class, clientId);
+            return client.getAppointmentList();
+            
+        } finally {
+            em.close();
+        }
+    }
+    public Appointment get(int appointmentId) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            Appointment appt = em.find(Appointment.class, appointmentId);
+            return appt;
+        } finally {
+            em.close();
+        }
+    }
     public void insert(Appointment appt) throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         EntityTransaction et = em.getTransaction();
@@ -19,7 +50,20 @@ public class AppointmentDB {
             em.persist(appt);
             em.merge(client);
             et.commit();
-            
+        } catch (Exception e) {
+            et.rollback();
+        } finally {
+            em.close();
+        }
+    }
+    public void update(Appointment appt) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        
+        try {
+            et.begin();
+            em.merge(appt);
+            et.commit();
         } catch (Exception e) {
             et.rollback();
         } finally {
