a38a62bb1f4a7ffba077533e0afb6f9856df7178
j-albento <joannamarie.albento@gmail.com>
2023-02-16 12:45:45 -0700

    Updated

--- MMT-Booking-System/src/java/services/AppointmentService.java
+++ MMT-Booking-System/src/java/services/AppointmentService.java
@@ -1,5 +1,50 @@
 package services;
 
+import dataaccess.AppointmentDB;
+import dataaccess.ServiceDB;
+import java.util.Date;
+import java.util.List;
+import models.Appointment;
+import models.Client;
+import models.Service;
+
 public class AppointmentService {
     
+    public List<Appointment> getAll() throws Exception {
+        AppointmentDB apptdb = new AppointmentDB();
+        
+        return apptdb.getAll();
+    }
+    // get all appointments associated with specific client
+    public List<Appointment> getAll(int clientId) throws Exception {
+        AppointmentDB apptdb = new AppointmentDB();
+        
+        return apptdb.getAll(clientId);
+    }
+    public Appointment get(int appointmentId) throws Exception {
+        AppointmentDB apptdb = new AppointmentDB();
+        Appointment appt = apptdb.get(appointmentId);
+        return appt;
+    }
+    
+    public void insert(Client client, int serviceId, String appointmentAddress) throws Exception {
+        ServiceDB servdb = new ServiceDB();
+        AppointmentDB apptdb = new AppointmentDB();
+     
+        Service service = servdb.getService(serviceId);
+        Appointment appt = new Appointment(0, client, service, true);
+        appt.setAppointmentAddress(appointmentAddress);
+        apptdb.insert(appt);
+    }
+    
+    public void delete(int appointmentId) throws Exception {
+        AppointmentDB apptdb = new AppointmentDB();
+        Appointment appt = apptdb.get(appointmentId);
+        
+        apptdb.delete(appt);
+    }
+    
+    public void update(Date appointmentDate, Date startTime, Date endTime, String address) throws Exception {
+        
+    }
 }
