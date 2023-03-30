cb3a50f7489ceb6301b9ed38bd5435fc78a74fd6
j-albento <joannamarie.albento@gmail.com>
2023-03-30 17:44:35 -0600

    Added authentication filter

---
+++ MMT-Booking-System/src/java/filters/AuthenticationFilter.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Account;
+
+/**
+ *
+ * @author Joanna
+ */
+public class AuthenticationFilter implements Filter {
+    
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+        
+        Account account = (Account) session.getAttribute("account");
+       
+        if (account == null) {
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        int validRole = account.getRole().getRoleId();
+        if (validRole != 1 || validRole != 2) {
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        
+        chain.doFilter(request, response);
+       
+    }
+  
+    /**
+     * Destroy method for this filter
+     */
+    @Override
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     * @param filterConfig
+     */
+    @Override
+    public void init(FilterConfig filterConfig) {        
+      
+    }
+
+   
+}
